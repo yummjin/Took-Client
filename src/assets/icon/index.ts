24d882a09ff41d9ad02b94f82d9326411c99bf3e
cho4u4o <cho4u4o@gmail.com>
2025-05-24 00:43:05 +0900

    feat : add icons for `NoticeScreen`

--- src/assets/icon/index.ts
+++ src/assets/icon/index.ts
@@ -1,5 +1,7 @@
 import UserIcon from './icon-user.png';
 import ResultIcon from './icon-result.svg';
 import NoticeIcon from './icon-notice.svg';
+import NotificationIcon from './icon-notification.svg';
+import MessageIcon from './icon-message.svg';
 
-export { UserIcon, ResultIcon, NoticeIcon };
+export { UserIcon, ResultIcon, NoticeIcon, NotificationIcon, MessageIcon };
