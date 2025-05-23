3751ecac77677e6451773c6971fa228204b429d7
cho4u4o <cho4u4o@gmail.com>
2025-05-23 14:10:50 +0900

    design : import stackflow css

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './global.css';
+import '@stackflow/plugin-basic-ui/index.css';
 import App from './App';
 
 createRoot(document.getElementById('root')!).render(
