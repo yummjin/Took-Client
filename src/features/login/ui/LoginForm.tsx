6566c84c68c091942a3eac9ea36bc9406f627fbf
cho4u4o <cho4u4o@gmail.com>
2025-05-23 23:32:04 +0900

    feat : add sample navigation logic to test

--- src/features/login/ui/LoginForm.tsx
+++ src/features/login/ui/LoginForm.tsx
@@ -1,6 +1,11 @@
+import { useFlow } from '@/app/stackflow';
+
+import { PATH } from '@/shared/constants';
 import { Button, Input } from '@/shared/ui';
 
 export default function LoginForm() {
+  const { replace } = useFlow();
+
   return (
     <form className="flex w-full flex-col gap-y-15">
       <div className="flex w-full flex-col gap-[10px]">
@@ -13,7 +18,7 @@ export default function LoginForm() {
         <Input placeholder="패스워드" intent="login" type="password" />
       </div>
       <div className="flex w-full flex-col gap-3">
-        <Button>학생으로 로그인</Button>
+        <Button onClick={() => replace(PATH.HOME, {})}>학생으로 로그인</Button>
         <Button intent="textmain" size="fit">
           관리자로 로그인
         </Button>
