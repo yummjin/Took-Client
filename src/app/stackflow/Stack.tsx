bffbde549107a9d51c248702826d14beb61801c5
cho4u4o <cho4u4o@gmail.com>
2025-05-23 17:16:24 +0900

    feat : implement `HomeScreen`

--- src/app/stackflow/Stack.tsx
+++ src/app/stackflow/Stack.tsx
@@ -1,11 +1,13 @@
 import { basicUIPlugin } from '@stackflow/plugin-basic-ui';
 import { basicRendererPlugin } from '@stackflow/plugin-renderer-basic';
 import { stackflow } from '@stackflow/react';
+
 import { LoginScreen } from '@/screen/login/ui';
+import { HomeScreen } from '@/screen/home/ui';
 
 export const { Stack, useFlow } = stackflow({
   transitionDuration: 350,
-  activities: { LoginScreen },
+  activities: { LoginScreen, HomeScreen },
   plugins: [
     basicRendererPlugin(),
     basicUIPlugin({
