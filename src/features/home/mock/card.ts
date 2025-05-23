2522e613f29486ee2a4f8c73179ff2fa9d7392b8
cho4u4o <cho4u4o@gmail.com>
2025-05-23 23:29:09 +0900

    feat : add mock data

---
+++ src/features/home/mock/card.ts
@@ -0,0 +1,32 @@
+import type { CardProps } from '@/features/home/types';
+
+export const CARD_MOCK: CardProps[] = [
+  {
+    id: 1,
+    title: '2025 총학생회 선거',
+    campus: '수원캠',
+    status: '진행중',
+    date: '2024.04.15 - 2024.04.16',
+  },
+  {
+    id: 2,
+    title: '제 2회 총무 선거',
+    campus: '수원캠',
+    status: '진행중',
+    date: '2024.04.15 - 2024.04.16',
+  },
+  {
+    id: 3,
+    title: '제 3회 기획 선거',
+    campus: '수원캠',
+    status: '진행중',
+    date: '2024.04.15 - 2024.04.16',
+  },
+  {
+    id: 4,
+    title: '제 3회 기획 선거',
+    campus: '수원캠',
+    status: '진행중',
+    date: '2024.04.15 - 2024.04.16',
+  },
+];
