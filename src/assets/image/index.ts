1f6c0406dc742ae2b947d60eab37090444eeb6b5
cho4u4o <cho4u4o@gmail.com>
2025-05-24 23:16:10 +0900

    feat : add background image asset of `VoteScreen`

--- src/assets/image/index.ts
+++ src/assets/image/index.ts
@@ -1,4 +1,6 @@
 import LoginBg from './bg-login.png';
 import HomeBg from './bg-home.png';
+import VoteBg from './bg-vote.png';
+import VotePromiseBg from './bg-vote-promise.png';
 
-export { LoginBg, HomeBg };
+export { LoginBg, HomeBg, VoteBg, VotePromiseBg };
