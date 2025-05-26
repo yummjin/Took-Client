11ce8a5e6e8b3e5402e7c07b78698e4828dc8e5c
cho4u4o <cho4u4o@gmail.com>
2025-05-26 15:01:28 +0900

    feat : connect `VoteCreateScreen` from `AdminHomeContainer`

--- src/widgets/admin-home/ui/AdminHomeContainer.tsx
+++ src/widgets/admin-home/ui/AdminHomeContainer.tsx
@@ -1,6 +1,9 @@
+import { useFlow } from '@/app/stackflow';
 import { CreateVoteIcon, DashBoardIcon } from '@/assets/icon';
+import { PATH } from '@/shared/constants';
 
 export default function AdminHomeContainer() {
+  const { push } = useFlow();
   return (
     <div className="p-normal size-full">
       <p className="mt-mt text-4xl font-bold">안녕하세요, 관리자님</p>
@@ -8,7 +11,10 @@ export default function AdminHomeContainer() {
         어떤 서비스를 찾으시나요?
       </p>
       <div className="mt-9 flex w-full flex-col gap-y-5">
-        <button className="border-m shadow-voteItem grid h-[232px] w-full cursor-pointer place-items-center rounded-lg border-[1px] bg-white/30">
+        <button
+          className="border-m shadow-voteItem grid h-[232px] w-full cursor-pointer place-items-center rounded-lg border-[1px] bg-white/30"
+          onClick={() => push(PATH.VOTE_CREATE, {})}
+        >
           <div className="relative flex h-[232px] w-[270px] flex-col items-center">
             <img src={CreateVoteIcon} className="mb-8 size-50" />
             <div className="bg-md absolute top-36 grid h-14 w-[270px] place-items-center rounded-md text-xl font-semibold text-white">
