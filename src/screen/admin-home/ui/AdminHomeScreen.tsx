688809a2fbb8e97a48292a4b7bbecf2a927162f8
cho4u4o <cho4u4o@gmail.com>
2025-05-28 11:35:22 +0900

    feat : implement logout functionality at admin home

--- src/screen/admin-home/ui/AdminHomeScreen.tsx
+++ src/screen/admin-home/ui/AdminHomeScreen.tsx
@@ -1,14 +1,18 @@
+import { useFlow } from '@/app/stackflow';
 import { VoteBg } from '@/assets/image';
+import { PATH } from '@/shared/constants';
 import { AdminAppBar } from '@/shared/ui';
 import { AdminHomeContainer } from '@/widgets/admin-home/ui';
 import { AppScreen } from '@stackflow/plugin-basic-ui';
 
 export default function AdminHomeScreen() {
+  const { replace } = useFlow();
+
   return (
     <AppScreen
       preventSwipeBack
       backgroundImage={`url(${VoteBg})`}
-      appBar={AdminAppBar}
+      appBar={AdminAppBar(() => replace(PATH.LOGIN, {}))}
     >
       <AdminHomeContainer />
     </AppScreen>
