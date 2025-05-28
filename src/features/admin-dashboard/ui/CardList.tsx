aa567aa7e84d618bf3dedaae7ec35ff31c2a8e7b
cho4u4o <cho4u4o@gmail.com>
2025-05-28 11:24:49 +0900

    feat : add mock data for admin dashboard

--- src/features/admin-dashboard/ui/CardList.tsx
+++ src/features/admin-dashboard/ui/CardList.tsx
@@ -1,33 +1,22 @@
+import type { VoteStatus } from '@/shared/types';
 import { Card } from '@/shared/ui';
+import { VOTE_MOCK } from '../mock';
 
-export default function CardList() {
+export default function CardList({ status }: { status: VoteStatus }) {
+  const data = VOTE_MOCK.filter(
+    ({ status: voteStatus }) => voteStatus === status,
+  );
   return (
     <div className="scrollbar-hide px-normal flex size-full flex-1 flex-col gap-y-[26px] overflow-scroll">
-      <Card
-        campus="서울"
-        title="골든글러브"
-        status="진행중"
-        date="000"
-        className="mt-11"
-      />
-      <Card
-        campus="서울"
-        title="골든글러브"
-        status="진행중"
-        date="2024.04.19 - 2024.04.24"
-      />
-      <Card
-        campus="서울"
-        title="골든글러브"
-        status="진행중"
-        date="2024.04.19 - 2024.04.24"
-      />
-      <Card
-        campus="서울"
-        title="골든글러브"
-        status="진행중"
-        date="2024.04.19 - 2024.04.24"
-      />
+      {data.map(({ id, campus, status, title, date }) => (
+        <Card
+          key={id}
+          campus={campus}
+          status={status}
+          title={title}
+          date={date}
+        />
+      ))}
     </div>
   );
 }
