d863558782b2ae63759783bb37d77fd9aa44493b
cho4u4o <cho4u4o@gmail.com>
2025-06-16 17:32:15 +0900

    feat : add character and kakao icon

--- src/assets/icon/index.ts
+++ src/assets/icon/index.ts
@@ -1,8 +1,11 @@
-import VerifiedCheckIcon from './icon-check-verified.svg';
 import Character from './character.svg';
+import Character2 from './character2.svg';
+import VerifiedCheckIcon from './icon-check-verified.svg';
 import CreateCompleteIcon from './icon-create-complete.png';
 import CreateVoteIcon from './icon-create-vote.webp';
 import DashBoardIcon from './icon-dashboard.webp';
+import BackButton from './icon-default-back.svg';
+import KakaoIcon from './icon-kakao.svg';
 import MessageIcon from './icon-message.svg';
 import NoticeIcon from './icon-notice.svg';
 import NotificationIcon from './icon-notification.svg';
@@ -11,14 +14,15 @@ import TextBoxIcon from './icon-textbox.svg';
 import UserIcon from './icon-user.png';
 import VoteCompleteIcon from './icon-vote-complete.png';
 import VoteIcon from './icon-vote.svg';
-import BackButton from './icon-default-back.svg';
 
 export {
   BackButton,
   Character,
-  CreateVoteIcon,
+  Character2,
   CreateCompleteIcon,
+  CreateVoteIcon,
   DashBoardIcon,
+  KakaoIcon,
   MessageIcon,
   NoticeIcon,
   NotificationIcon,
