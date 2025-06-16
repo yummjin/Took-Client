bcc7c2965e48d148c5e85d33eda4ed71721c3409
cho4u4o <cho4u4o@gmail.com>
2025-06-16 22:12:35 +0900

    design : fix scrollable and element height

--- src/widgets/entry/ui/EntryContainer.tsx
+++ src/widgets/entry/ui/EntryContainer.tsx
@@ -7,11 +7,11 @@ export default function EntryContainer() {
   const { replace } = useFlow();
 
   return (
-    <div className="flex size-full flex-col">
+    <div className="flex size-full flex-col overflow-hidden">
       <div className="-ml-[14px] w-[500px]">
         <img src={Character} className="h-full w-full object-cover" />
       </div>
-      <div className="absolute bottom-0 z-10 flex h-[582px] w-full flex-col items-center rounded-t-lg border-t-[2px] border-white bg-white/50 p-10 backdrop-blur-[2px]">
+      <div className="absolute bottom-0 z-10 flex h-[70%] w-full flex-col items-center rounded-t-lg border-t-[2px] border-white bg-white/50 p-10 backdrop-blur-[2px]">
         <div className="relative">
           <img src={TextBoxIcon} className="w-63" />
           <span className="absolute top-2.25 left-1/2 z-20 -translate-x-1/2 text-lg font-semibold text-nowrap text-white">
