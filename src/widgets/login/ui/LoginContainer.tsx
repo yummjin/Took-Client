1b979244fba1cd95f9db81fd17b5652abc20f23c
cho4u4o <cho4u4o@gmail.com>
2025-06-16 17:57:30 +0900

    feat : implement temporary navigation

--- src/widgets/login/ui/LoginContainer.tsx
+++ src/widgets/login/ui/LoginContainer.tsx
@@ -1,7 +1,11 @@
+import { useFlow } from '@/app/stackflow';
 import { Character2, KakaoIcon } from '@/assets/icon';
+import { PATH } from '@/shared/constants';
 import { Button } from '@/shared/ui';
 
 export default function LoginContainer() {
+  const { replace } = useFlow();
+
   return (
     <div className="p-normal flex size-full flex-col space-y-15">
       <p className="mt-mt text-4xl leading-snug font-semibold">
@@ -14,6 +18,7 @@ export default function LoginContainer() {
       <Button
         intent="kakao"
         className="flex items-center justify-center gap-2 py-[14px] text-lg"
+        onClick={() => replace(PATH.HOME, {})}
       >
         <img src={KakaoIcon} />
         카카오 로그인
