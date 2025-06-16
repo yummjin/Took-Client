a2363465ec60e188ee4010a0955d3b7291cf4e4f
cho4u4o <cho4u4o@gmail.com>
2025-06-16 16:26:01 +0900

    feat : add assets

--- src/assets/icon/index.ts
+++ src/assets/icon/index.ts
@@ -1,4 +1,5 @@
 import VerifiedCheckIcon from './icon-check-verified.svg';
+import Character from './character.svg';
 import CreateCompleteIcon from './icon-create-complete.png';
 import CreateVoteIcon from './icon-create-vote.webp';
 import DashBoardIcon from './icon-dashboard.webp';
@@ -6,6 +7,7 @@ import MessageIcon from './icon-message.svg';
 import NoticeIcon from './icon-notice.svg';
 import NotificationIcon from './icon-notification.svg';
 import ResultIcon from './icon-result.svg';
+import TextBoxIcon from './icon-textbox.svg';
 import UserIcon from './icon-user.png';
 import VoteCompleteIcon from './icon-vote-complete.png';
 import VoteIcon from './icon-vote.svg';
@@ -13,6 +15,7 @@ import BackButton from './icon-default-back.svg';
 
 export {
   BackButton,
+  Character,
   CreateVoteIcon,
   CreateCompleteIcon,
   DashBoardIcon,
@@ -20,6 +23,7 @@ export {
   NoticeIcon,
   NotificationIcon,
   ResultIcon,
+  TextBoxIcon,
   UserIcon,
   VerifiedCheckIcon,
   VoteCompleteIcon,
