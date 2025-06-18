b283eeebfde790a2e9ff2503a6dde02cab47f66a
cho4u4o <cho4u4o@gmail.com>
2025-06-18 21:14:46 +0900

    feat : change icon and add function to home appbar

--- src/screen/home/ui/HomeScreen.tsx
+++ src/screen/home/ui/HomeScreen.tsx
@@ -3,13 +3,20 @@ import { AppScreen } from '@stackflow/plugin-basic-ui';
 import { HomeBg } from '@/assets/image';
 import { HomeAppBar } from '@/shared/ui';
 import { HomeContainer } from '@/widgets/home/ui';
+import { useFlow } from '@/app/stackflow';
+import { PATH } from '@/shared/constants';
 
 export default function HomeScreen() {
+  const { replace } = useFlow();
+
   return (
     <AppScreen
       preventSwipeBack
       backgroundImage={`url(${HomeBg})`}
-      appBar={HomeAppBar}
+      appBar={HomeAppBar(
+        () => replace(PATH.LOGIN, {}),
+        () => {},
+      )}
     >
       <HomeContainer />
     </AppScreen>
